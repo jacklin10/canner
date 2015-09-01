ee05514035be703f6538daa6f55c3f44d7d3b638
Matt McMahand <matthew.mcmahand@wellsfargo.com>
2015-09-01 09:11:52 -0700

    cleaner prepare method

--- lib/canner/util.rb
+++ lib/canner/util.rb
@@ -3,23 +3,7 @@ class Util
   # ensures whatever is passed in comes out an array of symbols
   class << self
     def prepare(str)
-      symbolize(arrayify(str).flatten)
-    end
-
-    # ensures the array elements are symbols
-    def symbolize(strings)
-      strings.map{|s| s.to_sym}
-    end
-
-    # ensure given roles are in the form of an array
-    def arrayify(roles)
-      if roles.nil?
-        []
-      elsif roles.respond_to?(:to_ary)
-        roles.to_ary || [roles]
-      else
-        [roles]
-      end
+      Array(str).flatten.map(&:to_sym)
     end
   end
 end
