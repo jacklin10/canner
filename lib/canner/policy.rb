2def795fb220ed1bb76b795fd010fb62a76ed9c9
jacklin <jacklin10@gmail.com>
2017-11-16 09:42:10 -0500

    unprotected has_role? commenting and some better naming

--- lib/canner/policy.rb
+++ lib/canner/policy.rb
@@ -2,6 +2,12 @@ module Canner
 
   class Policy
 
+    # current_user - The user whose access you are checking
+    # current_branch - The optional 3rd access checking item.
+    #                  With branch you are saying can the user access this resource in this case.
+    #                  For example, is Joe a manager at the Pittsburgh branch?
+    #                  He may be a manager in Pittsburgh, but not in Monaca so you need to know the branch.
+    # method         - The method the user is attempting to use. ex: :index, :create, :destroy, :custom_name
     def initialize(current_user, method, current_branch=nil)
       @current_user = current_user
       @current_branch = current_branch
@@ -11,14 +17,14 @@ module Canner
 
     # if you handle your roles differently you'll need to override.
     # use: rails g canner:fetch_roles
-    # expects an array or strings or symbols that represent the user roles
+    # It must return either an array of strings/symbols, or an array of Role objects that have a 'name' method.
     def fetch_roles
       @current_user.nil? ? [] : @current_user.roles
     end
 
     # implement in your policy class to auto scope in an action
     def canner_scope
-      raise ArgumentError.new("NOT IMPLEMENTED")
+      raise ArgumentError, 'NOT IMPLEMENTED'
       # ex:
       # case @method
       # when :index
@@ -31,7 +37,7 @@ module Canner
     # implement in your policy class.
     # return true when the user can access the action or resource and false when they can't
     def can?
-      raise ArgumentError.new("NOT IMPLEMENTED")
+      raise ArgumentError, 'NOT IMPLEMENTED'
       # ex:
       # case @method
       # when :index, :show
@@ -41,23 +47,23 @@ module Canner
       # end
     end
 
+    # accepts array of string, symbols or a mix:  [:admin, 'guest']
+    # also accepts a list of params as in has_role?(:admin, :guest)
+    def has_role?(check_roles)
+      begin
+        @roles.any? do |r|
+          user_role = r.respond_to?(:name) ? r.name : r.to_s
+          Util.prepare(check_roles).include?(user_role.to_sym)
+        end
+      rescue StandardError => e
+        raise ArgumentError.new "Canner: Problem fetching user roles. If current_user.roles isn't how you do it see wiki for overriding fetch_roles. #{e.message}"
+      end
+    end
+
     protected
 
     def is_method?(methods)
       prepare(methods).include?(@method)
     end
-
-    def has_role?(roles)
-      begin
-        @roles.any? do |r|
-          name = r.respond_to?(:name) ? r.name : r.to_s
-          Util.prepare(roles).include?(name.to_sym)
-        end
-      rescue Exception => e
-        raise ArgumentError.new "Canner: Problem fetching user roles. If current_user.roles isn't how you do it see wiki for overriding fetch_roles. #{e}"
-      end
-    end
-
   end
-
 end
